70134bdd1750e005ac9f28bb28e3c02988e91025
Gawsan <gawsan98@gmail.com>
2022-01-19 22:31:18 +0530

    function added

--- app/src/main/java/com/gawsan/learnera/checkSum.java
+++ app/src/main/java/com/gawsan/learnera/checkSum.java
@@ -2,24 +2,20 @@ package com.gawsan.learnera;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.webkit.WebView;
 
 public class checkSum extends AppCompatActivity {
     private WebView gaws;
     private Object view;
+    private Button btn;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_sum);
-
         gaws.canGoBack();
-        private void checksum(){
-            gaws.getElevation();
-
-        }
-
     }
 }
